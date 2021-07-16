eb6c34cf4d029c9c5eda10dc24eab2a80c01fffc
Pranav Addepalli <pranav.addepalli@gmail.com>
2021-07-15 21:42:51 -0400

    created offline exe version

---
+++ offline/main.py
@@ -0,0 +1,11 @@
+import keyboard
+
+keyboard.add_abbreviation('ii', 'y')
+keyboard.add_abbreviation('7', 't')
+keyboard.add_abbreviation('0', 'o')
+keyboard.add_abbreviation(')', 'O')
+keyboard.add_abbreviation('&', 'T')
+keyboard.add_abbreviation('*', 'Y')
+
+# Block forever, like `while True`.
+keyboard.wait()
